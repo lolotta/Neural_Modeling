ea113d7811ef99f2a275cac34ec85bfcc143a115
lolotta <64040464+lolotta@users.noreply.github.com>
2024-01-11 16:51:48 +0100

    update plots and make subject name into the file names

--- HW5/plot.py
+++ HW5/plot.py
@@ -5,9 +5,11 @@ import seaborn as sns
 a1, a2, a3, a4, a5, a6 = 0, 40, 80, 120, 160, 200
 collect_attempts = [a2, a3, a4, a5, a6]
 string_attempts = ['No Perturbation', 'Gradual Perturbation', 'No Perturbation', 'Sudden Perturbation', 'Random Perturbation']
-subject_name = "subject"
+subject_name = "lotta"
 attempts = 200
-data = pd.read_csv("error_angles_"+ subject_name + ".csv")
+import os
+print(os.getcwd())
+data = pd.read_csv("HW5/error_angles_"+ subject_name + ".csv")
 
 g0 = sns.scatterplot(data = data, x = 'attempts', y = 'error_angles') 
 g0 = sns.lineplot(data = data, x = 'attempts', y = 'error_angles')
@@ -19,4 +21,4 @@ for i in range(len(collect_attempts)):
 g0.set(xlabel='attempts', ylabel='error_angles', title = 'Error_angles over attempts')
 plt.show()
 
-g0.figure.savefig("error_angles_"+ subject_name + "png.")
+g0.figure.savefig("HW5/error_angles_"+ subject_name + ".png")
